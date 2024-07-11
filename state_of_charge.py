873f475f335ac65c452cf333dc2cac0828dc66d4
Jakob Malm <jakob.malm@gmail.com>
2024-07-11 22:20:29 +0200

    Only present two decimals + reformat

--- state_of_charge.py
+++ state_of_charge.py
@@ -14,28 +14,38 @@ class StateOfChargeCalculator(hass.Hass):
         self.charger_energy_entity = self.get_entity(self.charger_energy_entity_id)
         self.last_known_state_of_charge_entity = self.get_entity(self.last_known_state_of_charge_entity_id)
         self.estimated_state_of_charge_entity = self.get_entity(self.estimated_state_of_charge_entity_id)
-    
+
         # Register callbacks
         self.listen_state(self.estimate, self.charger_energy_entity_id)
         self.listen_state(self.estimate, self.last_known_state_of_charge_entity_id)
 
+        # Do the calculation (mainly for development purposes)
+        self.estimate(None, None, None, None, None)
+
 
     def estimate(self, entity, attribute, old, new, kwargs):
+        # TODO: What if the car has been disconnected since last known state?
+        # Should we reset the last known state?
+        # Should we adjust estimation depending on how much time has passed since the car was disconnected?
+        # Should we clear the state (if that's even possible)?
         estimated_soc = self.estimate_state_of_charge(float(self.last_known_state_of_charge_entity.state),
                                                  parser.parse(self.last_known_state_of_charge_entity.last_changed),
                                                  float(self.battery_size_kWh),
                                                  self.charger_energy_entity)
         self.estimated_state_of_charge_entity.set_state(state=round(estimated_soc))
 
-    def estimate_state_of_charge(self, known_state_of_charge: float, last_updated: datetime, battery_size_kWh: float, charger_energy_entity: Entity) -> float:
-        """Estimate the state of charge right now, based on last known state of charge and the charger energy consumption."""
-        state_of_charge_kWh = known_state_of_charge / 100 * battery_size_kWh
-        
+    def estimate_state_of_charge(self, known_state_of_charge: float, last_updated: datetime,
+                                 battery_size_kwh: float, charger_energy_entity: Entity) -> float:
+        """Estimate the state of charge right now, based on last known state of charge and the charger energy
+         consumption.
+         """
+        state_of_charge_kwh = known_state_of_charge / 100 * battery_size_kwh
+
         # Add charger energy consumption since last known state of charge.
-        charged_kWh = self.charger_used_energy_since(charger_energy_entity, last_updated) # Assumes no other vehicle has used the charger since *last_time*.
-        self.log(f"Charged since {last_updated}: {charged_kWh} kWh")
-        new_state_of_charge_kWh = state_of_charge_kWh + charged_kWh
-        new_state_of_charge = new_state_of_charge_kWh / battery_size_kWh * 100
+        charged_kwh = self.charger_used_energy_since(charger_energy_entity, last_updated) # Assumes no other vehicle has used the charger since *last_time*.
+        self.log(f"Charged since {last_updated}: {charged_kwh:.2f} kWh")
+        new_state_of_charge_kwh = state_of_charge_kwh + charged_kwh
+        new_state_of_charge = new_state_of_charge_kwh / battery_size_kwh * 100
 
         return new_state_of_charge
 
@@ -46,7 +56,7 @@ class StateOfChargeCalculator(hass.Hass):
         # TODO: interpolate between
         if not energy_history:
             return 0
-        
+
         earliest_state_after_time = float(energy_history[0]['state'])
         latest_state = float(energy_history[-1]['state'])
         energy_consumed = latest_state - earliest_state_after_time
